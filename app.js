390f4732fc1831c8ba05e939ec51e1d4a0417bd7
Caiii-d <113438605+Caiii-d@users.noreply.github.com>
2022-10-27 13:10:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ const APP = {
          console.log("data",data);
 
         url = data.name;
-        var chk = "^https://(?:[^.]+.)?ziwen.ibgang.com/.*$";
+        var chk = "ziwen.ibgang.com/.*$";
         var regex = new RegExp(chk);
 
         console.log("regex",regex);
